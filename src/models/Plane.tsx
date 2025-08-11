e6c151d923be7c93d492db9cd1bb9b3a0c142238
fharten <florian.harten@pm.me>
2025-08-11 21:12:38 +0200

    able to interact with homescreen

---
+++ src/models/Plane.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import * as THREE from 'three';
+
+import planeScene from '../assets/3d/plane.glb';
+
+interface PageProps {
+  isRotating: boolean;
+  position: [number, number, number];
+  rotation: [number, number, number];
+  scale: [number, number, number];
+}
+
+// 3D Model from: https://sketchfab.com/3d-models/stylized-ww1-plane-c4edeb0e410f46e8a4db320879f0a1db
+export function Plane({ isRotating, ...props }: PageProps) {
+  const ref = useRef<THREE.Mesh>(null);
+  // Load the 3D model and its animations
+  const { scene, animations } = useGLTF(planeScene);
+  // Get animation actions associated with the plane
+  const { actions } = useAnimations(animations, ref);
+
+  // Use an effect to control the plane's animation based on 'isRotating'
+  // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
+  useEffect(() => {
+    if (isRotating) {
+      actions['Take 001']?.play();
+    } else {
+      actions['Take 001']?.stop();
+    }
+  }, [actions, isRotating]);
+
+  return (
+    <mesh {...props} ref={ref}>
+      // use the primitive element when you want to directly embed a complex 3D
+      model or scene
+      <primitive object={scene} />
+    </mesh>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,99 @@
+import { Canvas } from '@react-three/fiber';
+import { Suspense, useRef, useState } from 'react';
+
+import sakura from '../assets/sakura.mp3';
+import Loader from '../components/Loader';
+import Bird from '../models/Bird';
+import Sky from '../models/Sky';
+import { Island } from '../models/Island';
+import { Plane } from '../models/Plane';
+
+const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
+
+  const [currentStage, setCurrentStage] = useState<number | null>(null);
+  const [isRotating, setIsRotating] = useState<boolean>(false);
+
+  const adjustBiplaneForScreenSize = () => {
+    let screenScale: [number, number, number],
+      screenPosition: [number, number, number];
+
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale: [number, number, number],
+      screenPosition: [number, number, number];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
+  return (
+    <section className='w-full h-screen relative'>
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? 'cursor-grabbing' : 'cursor-grab'
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            color='#b1e1ff'
+            groundColor='#000000'
+            intensity={1}
+          />
+
+          <Bird />
+          <Sky isRotating={isRotating} />
+          <Island
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+            position={islandPosition}
+            rotation={[0.1, 4.7077, 0]}
+            scale={islandScale}
+          />
+          <Plane
+            isRotating={isRotating}
+            position={biplanePosition}
+            rotation={[0, 20.1, 0]}
+            scale={biplaneScale}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
